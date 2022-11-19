d1e443604d674b5b2d5e2e679f8d1a4fb0b2cf14
Aigerim Erkebaeva <erkebaeva.aigerim88@gmail.com>
2022-11-19 09:49:29 +0600

    feat: make layout of project and team sections

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     nextArrow:
-      '<button type="button" class="slick-arrow slick-next"><img src="../images/next.svg" alt="next arrow" /></button>',
+      '<button type="button" class="slick-arrow slick-next"><img src="images/next.png" alt="next arrow" /></button>',
     prevArrow:
-      '<button type="button" class="slick-arrow slick-prev"><img src="../images/prev.svg" alt="prev arrow" /></button>',
+      '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.png" alt="prev arrow" /></button>',
     responsive: [
       {
         breakpoint: 1024,
